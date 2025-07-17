e37f023985b3e843e17a40c9096e71d57c938fd1
Oleksii Karpenko <alexkarpenko@yahoo.com>
2025-07-17 22:24:03 +0300

    fix: Revert to use original credentials caching approach

--- src/signer.rs
+++ src/signer.rs
@@ -1,9 +1,10 @@
-use crate::{config::Config, credentials::CachedCredentials};
-use aws_credential_types::Credentials;
+use crate::config::Config;
+use aws_credential_types::provider::ProvideCredentials;
 use aws_sigv4::{
     http_request::{PayloadChecksumKind, SignableRequest, SigningInstructions, SigningSettings, sign},
     sign::v4,
 };
+use aws_smithy_runtime_api::client::identity::Identity;
 use std::{
     sync::Arc,
     time::{Duration, SystemTime},
@@ -14,18 +15,18 @@ use tracing::debug;
 pub struct ProxySigner {
     service: String,
     region: String,
-    credentials_provider: Arc<CachedCredentials>,
+    credentials_provider: Arc<Box<dyn ProvideCredentials>>,
     signing_settings: SigningSettings,
 }
 
 impl ProxySigner {
-    pub fn new(config: &Config, credentials_provider: CachedCredentials) -> Self {
+    pub fn new(config: &Config, credentials_provider: Box<dyn ProvideCredentials>) -> Self {
         let service = config.service.as_ref().unwrap().clone();
         let region = config.region.as_ref().unwrap().clone();
-        let expiring_timeout = Duration::from_secs(config.signature_lifetime.unwrap_or(config.request_timeout));
+        let signature_lifetime = Duration::from_secs(config.signature_lifetime.unwrap_or(config.request_timeout));
 
         let mut signing_settings = SigningSettings::default();
-        signing_settings.expires_in = Some(expiring_timeout);
+        signing_settings.expires_in = Some(signature_lifetime);
         signing_settings.payload_checksum_kind = PayloadChecksumKind::XAmzSha256;
 
         Self {
@@ -36,26 +37,16 @@ impl ProxySigner {
         }
     }
 
-    #[inline]
-    async fn provide_credentials(&self) -> anyhow::Result<Credentials> {
-        let creds = self.credentials_provider.provide_credentials().await?;
-        debug!(expiry = ?(creds.expiry().unwrap_or_else(SystemTime::now).duration_since(SystemTime::now())?));
-        Ok(creds)
-    }
-
-    #[inline]
-    fn signing_settings(&self) -> SigningSettings {
-        self.signing_settings.clone()
-    }
-
     pub async fn sign(&self, req: SignableRequest<'_>) -> anyhow::Result<SigningInstructions> {
-        let identity = self.provide_credentials().await?.into();
+        let identity: Identity = self.credentials_provider.provide_credentials().await?.into();
+        debug!(expiry = ?(identity.expiration().unwrap_or_else(SystemTime::now).duration_since(SystemTime::now())?));
+
         let params = v4::SigningParams::builder()
             .identity(&identity)
             .region(&self.region)
             .name(&self.service)
             .time(SystemTime::now())
-            .settings(self.signing_settings())
+            .settings(self.signing_settings.clone())
             .build()?
             .into();
 
